dc681455ad01d77ab6e9f456ca58ff5eb09e1b4c
neonoclock <neonoclock213@naver.com>
2026-03-16 00:47:28 +0900

    perf: GA 스크립트 로딩 전략을 lazyOnload로 변경해 초기 로드 성능 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,9 +58,9 @@ export default function RootLayout({
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-              strategy="afterInteractive"
+              strategy="lazyOnload"
             />
-            <Script id="ga-init" strategy="afterInteractive">
+            <Script id="ga-init" strategy="lazyOnload">
               {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
